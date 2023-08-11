abde8c99021968b1e6459b3c4da41336be633b08
RJ-TUNNEL <141926220+RJ-TUNNEL@users.noreply.github.com>
2023-08-11 16:09:58 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -2,7 +2,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Auther  : Andy Yuda
+# Auther  : Bang RJ
 # (C) Copyright 2022
 # =========================================
 # // Export Color & Information
@@ -126,15 +126,15 @@ install_ssl(){
 
 # install nginx
 mkdir -p /home/vps/public_html
-wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/RJ-VPN/xraylite/main/vps.conf.txt"
+wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/RJ-TUNNEL/xraylite/main/vps.conf.txt"
 sleep 1
-wget -q -O xraymode.sh https://raw.githubusercontent.com/RJ-VPN/xraylite/main/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
+wget -q -O xraymode.sh https://raw.githubusercontent.com/RJ-TUNNEL/xraylite/main/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
 sleep 1 
-wget -q -O /etc/xray/config.json "https://raw.githubusercontent.com/RJ-VPN/xraylite/main/conf/config.json"
+wget -q -O /etc/xray/config.json "https://raw.githubusercontent.com/RJ-TUNNEL/xraylite/main/conf/config.json"
 chmod +x /etc/xray/config.json
 sleep 1 
 rm -f /etc/nginx/conf.d/xray.conf
-wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/RJ-VPN/xraylite/main/conf/xray.conf"
+wget -q -O /etc/nginx/conf.d/xray.conf "https://raw.githubusercontent.com/RJ-TUNNEL/xraylite/main/conf/xray.conf"
 chmod +x /etc/nginx/conf.d/xray.conf
 
 # Installing Xray Service
